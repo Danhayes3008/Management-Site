d31a6d86e58ae3133af7ad80b30834ce42f873ef
danhayes <danhayes3008@outlook.com>
2020-09-28 14:16:34 +0100

    started working on the login page backend

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html')
+    login_form = LoginForm(request.POST)
+    context = {'login_form': login_form}
+    return render(request, 'login.html', context)
