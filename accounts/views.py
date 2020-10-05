312306e3fe9094d051aa4d6eab2bfbffffc936f7
danhayes <danhayes3008@outlook.com>
2020-10-05 12:07:01 +0100

    added the register function so that new users can login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from .forms import LoginForm
+from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
 
@@ -32,3 +32,26 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "Thank you for visiting, please come again")
     return redirect(reverse('login'))
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = RegistrationForm(request.POST)  
+              
+        if registration_form.is_valid():
+            registration_form.save() 
+                       
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "Welcome to you're time management dashboard")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Something went wrong, please try again")
+    else:
+        registration_form = RegistrationForm()
+    return render(request, 'register.html',
+                  {"registration_form": registration_form})
