dfd29488e648c7c900df2603b25e373ee6f2c36f
danhayes <danhayes3008@outlook.com>
2020-10-05 11:03:20 +0100

    finished working on the login backend so that users can now login to the dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 from .forms import LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
+
 
 # Create your views here.
 
 def login(request):
-    login_form = LoginForm(request.POST)
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = LoginForm()
     context = {'login_form': login_form}
     return render(request, 'login.html', context)
