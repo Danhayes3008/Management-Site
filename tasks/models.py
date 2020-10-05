0f2cd5175145d3600af67b9a8c5ddce030f6dd6e
danhayes <danhayes3008@outlook.com>
2020-10-05 13:44:01 +0100

    created the task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('Complete', 'Complete'),
+        ('To Do', 'To Do')
+    )
+    name = models.CharField(max_length=250, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='To Do')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, default=User)
+
+    def __str__(self):
+        return self.user.username
