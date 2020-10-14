588d314184b2b06fd1b8138851140adcfb44f030
danhayes <danhayes3008@outlook.com>
2020-10-14 11:22:32 +0100

    added the view for my add task form

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from .forms import *
 
 # Create your views here.
 def addTask(request):
-    return render(request, 'addTask.html')
+    if request.method == 'POST':
+        task_form = taskForm(request.POST)
+        
+        if task_form.is_valid():
+            task_form.save()
+            messages.success(request, "Your task has been added")
+            return redirect('index')
+    else:
+        task_form = taskForm()
+    return render(request, 'addTask.html', {'task_form': task_form})
