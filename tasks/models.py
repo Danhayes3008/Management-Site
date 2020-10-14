a2d94658af53548f6d301281f458fdc5d40ca9df
danhayes <danhayes3008@outlook.com>
2020-10-14 12:15:50 +0100

    updated the model to meet my needs better

--- tasks/models.py
+++ tasks/models.py
@@ -3,13 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    STATUS_CHOICES = (
-        ('Complete', 'Complete'),
-        ('To Do', 'To Do')
-    )
-    name = models.CharField(max_length=250, null=True, blank=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='To Do')
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, default=User)
+    title = models.CharField(max_length=250, null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+        return self.title
