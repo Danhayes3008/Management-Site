a94c9eaf3bbd5b2d5fa609084d40e588ad746a8f
danhayes <danhayes3008@outlook.com>
2020-10-14 12:43:44 +0100

    connected the dashboard to the tasks list

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from tasks.models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context)
